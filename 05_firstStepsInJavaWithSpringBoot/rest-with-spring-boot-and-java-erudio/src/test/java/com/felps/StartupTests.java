259f484eb6fa4042e3ce3cb82333fca5f4055320
Felps <fabiofelipevianasantosfacul@gmail.com>
2024-06-07 15:04:31 -0300

    First Steps in Java with Spring Boot3

---
+++ 05_firstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/com/felps/StartupTests.java
@@ -0,0 +1,13 @@
+package com.felps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
